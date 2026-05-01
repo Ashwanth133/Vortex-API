0bf67b1d2bb539c17794bf9bbd605ec8a56fae16
Ashwanth133 <ashwanthashu133@gmail.com>
2026-05-01 20:23:46 +0530

    Updated Deployment

--- backend/src/main/java/com/ashwanth/ApKeyManager/Config/CorsConfig.java
+++ backend/src/main/java/com/ashwanth/ApKeyManager/Config/CorsConfig.java
@@ -6,11 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins(
+                        "http://localhost:8081",
+                        "https://tanstack-start-app.vortex-api.workers.dev",
+                        "https://vortex-api.ashw.tech",
+                        "https://api.ashw.tech"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
