98fe028df79c8c882a9e84d747e56c237bf2c157
Ashwanth133 <ashwanthashu133@gmail.com>
2026-05-01 14:09:54 +0530

    added comments

--- backend/src/main/java/com/ashwanth/ApKeyManager/ApKeyManagerApplication.java
+++ backend/src/main/java/com/ashwanth/ApKeyManager/ApKeyManagerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApKeyManagerApplication {
 
+	//main class
 	public static void main(String[] args) {
 		SpringApplication.run(ApKeyManagerApplication.class, args);
 	}
